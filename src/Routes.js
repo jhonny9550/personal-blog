ac24aef4c314da3c1bcfafab798115064b652704
Jhonny Martinez <jmartinez@ideabti.com>
2020-03-10 19:58:57 +1100

    Initial project setup

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './home/Home';
+import Blog from './blog/Blog';
+import Projects from './projects/Projects';
+
+export const ROUTE_NAMES = {
+  HOME: '/',
+  BLOG: '/blog',
+  PROJECTS: '/projects',
+  NOT_FOUND: '/404'
+};
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path={ROUTE_NAMES.HOME} component={Home} />
+      <Route path={ROUTE_NAMES.BLOG} component={Blog} />
+      <Route path={ROUTE_NAMES.PROJECTS} component={Projects} />
+      <Route path={ROUTE_NAMES.NOT_FOUND}>
+        <h1>Page not found :(</h1>
+      </Route>
+    </Switch>
+  );
+};
+
+export default Routes;
