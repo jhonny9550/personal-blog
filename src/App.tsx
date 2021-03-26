4c9559e4b4bff2133288fdf8f9d5d56972fd1770
Jhonny Martinez <jhonny9550@gmail.com>
2021-03-27 09:12:28 +1100

    #5 (Setup UI Kit using Tailwindcss)
    Button styled-components added
    
    TODO: Update default and black button variations

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from "react";
+import { ReactComponent as SearchIcon } from "./assets/icons/search.svg";
 
 const App = () => (
   <div className="font-sans">
-    <p className="heading">Travelers</p>
-    <p className="title">Heading Australia 🇦🇺</p>
-    <p className="lbody">The beginning:</p>
-    <p className="body">
-      Here is the story about how I came from Colombia to Australia
-    </p>
+    <h1>Travelers</h1>
+    <h2>Heading Australia 🇦🇺</h2>
+    <h3>The beginning:</h3>
+    <p>Here is the story about how I came from Colombia to Australia</p>
     <p className="sbody">Shhhh... don't share it yet.</p>
-    <button className="button">Share</button>
+    <button className="btn-blue" disabled>
+      Share
+    </button>
+    <button className="btn-blue-outlined" disabled>
+      Share
+    </button>
+    <button className="btn-yellow">Share</button>
+    <button className="btn-yellow-outlined">Share</button>
+    <button className="btn-yellow-outlined btn-small">Share</button>
+    <button className="btn-black btn-large">Share</button>
+    <button className="btn-default btn-large">Share</button>
+    <button className="btn-default-outlined btn-large">Share</button>
+    <button className="btn-blue btn-fab-small" disabled>
+      <SearchIcon />
+    </button>
+    <button className="btn-yellow btn-fab" disabled>
+      <SearchIcon />
+    </button>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,42 @@ module.exports = {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
       },
+      colors: {
+        yellow: {
+          dark: "#c7a400",
+          DEFAULT: "#FFD500",
+          light: "#ffff52",
+        },
+        blue: {
+          dark: "#0031cb",
+          DEFAULT: "#395AFF",
+          light: "#7f87ff",
+        },
+        black: {
+          DEFAULT: "#000000",
+        },
+        red: {
+          DEFAULT: "#FE0000",
+        },
+        salmon: {
+          DEFAULT: "#FE5F5F",
+        },
+        grey: {
+          dark: "#707070",
+          DEFAULT: "#9F9F9F",
+          light: "#E9E9E9",
+        },
+      },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ["disabled"],
+      backgroundColor: ["disabled"],
+      cursor: ["disabled"],
+      textColor: ["disabled"],
+      borderWidth: ["disabled"],
+    },
   },
   plugins: [],
 };
