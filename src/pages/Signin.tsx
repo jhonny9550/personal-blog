60106fe2da6b601cc583f5d042e5ec64f264b472
Jhonny Martinez <jhonny9550@gmail.com>
2021-03-28 07:55:15 +1100

    #5 #6 (Authentication, Setup UI Kit...)
    Input checkbox styles fixed
    
    Remember action added to signin

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "../components/Button";
 import HomeTitle from "../components/HomeTitle";
 import TextInput from "../components/TextInput";
 
 const Signin = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [remember, setRemember] = useState(false);
+
+  const handleOnChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setRemember(e.target.checked);
+    },
+    []
+  );
+
   return (
     <div className="mx-72">
       <HomeTitle />
@@ -16,6 +25,15 @@ const Signin = () => {
           type="password"
           className="mt-2"
         />
+        <label className="inline-flex items-center mt-3">
+          <input
+            type="checkbox"
+            className="form-checkbox h-5 w-5 text-gray-600"
+            checked={remember}
+            onChange={handleOnChange}
+          />
+          <span className="sbody ml-2">Remember me</span>
+        </label>
         <Button
           loading={loading}
           disabled={loading}
